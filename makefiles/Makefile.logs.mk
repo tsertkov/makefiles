a700214e90bd0610a0dc00ffb344e16f5e2baf22
Aleks Tsertkov <tsertkov@gmail.com>
2024-03-13 09:43:30 +0100

    Ad logs-api, refactor variables

--- makefiles/Makefile.logs.mk
+++ makefiles/Makefile.logs.mk
@@ -3,28 +3,40 @@
 
 .PHONY: *
 
+logs-api: SINCE ?= 1d
+logs-api: FILTER ?= { $$.httpMethod = "*" }
+logs-api: \
+	require-aws \
+	assert-LOGS_GROUP \
+	assert-SINCE \
+	assert-FILTER
+	$(call _announce_target, $@)
+	aws logs tail \
+		$(LOGS_GROUP) \
+		--format json \
+		--since $(SINCE) \
+		--filter '$(FILTER)' \
+		--follow
+
 logs-lambda: SINCE ?= 1d
 logs-lambda: FILTER ?= { $$.level = "*" }
 logs-lambda: \
 	require-aws \
-	assert-LAMBDA_NAME_PREFIX \
+	assert-LOGS_GROUP_PREFIX \
 	assert-LAMBDA_NAME \
 	assert-SINCE \
 	assert-FILTER
-
 	$(call _announce_target, $@)
-	aws logs tail \
-		$(LAMBDA_NAME_PREFIX)-$(LAMBDA_NAME) \
+	@aws logs tail \
+		$(LOGS_GROUP_PREFIX)-$(LAMBDA_NAME) \
 		--format json \
 		--since $(SINCE) \
-		--follow \
-		--filter '$(FILTER)'
+		--filter '$(FILTER)' \
+		--follow
 
-logs-access-sync: LOGS_BUCKET = $(shell $(MAKE) infra-outputs | jq -r '.[] | select(.Key=="LogsBucket").Value')
 logs-access-sync: \
 	require-aws \
 	assert-LOGS_BUCKET
-
 	$(call _announce_target, $@)
 	@aws s3 sync s3://$(LOGS_BUCKET) ./logs/$(ENV)
 
@@ -34,7 +46,6 @@ logs-access-show: \
 	require-gzcat \
 	require-open \
 	assert-ENV
-
 	$(call _announce_target, $@)
 	@find logs/$(ENV) -name "*.gz" | xargs gzcat | \
 		goaccess \
